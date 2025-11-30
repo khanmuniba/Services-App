e5046105babab2d87d9f5aec5b0b3ea95244c5ad
Muniba <munibak428@gmail.com>
2025-11-30 13:11:02 +0500

    changes made

--- configs/db.js
+++ configs/db.js
@@ -4,8 +4,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  
+     
     });
     console.log("✅ MongoDB connected");
         console.log("URI:", process.env.MONGO_URI); 
